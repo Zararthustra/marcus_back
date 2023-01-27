b0ee03672a62c099e019854448127a2b3818ab8a
Zararthustra <70105093+Zararthustra@users.noreply.github.com>
2023-01-27 21:20:21 +0100

    Create a base model

--- src/marcus/models.py
+++ src/marcus/models.py
@@ -3,45 +3,33 @@ from django.db import models
 import uuid
 
 
-class Critic(models.Model):
+class MovieBaseModel(models.Model):
     id = models.UUIDField(primary_key=True, editable=False, default=uuid.uuid4)
-    content = models.CharField(max_length=1000)
-    user = models.ForeignKey(settings.AUTH_USER_MODEL,
-                              on_delete=models.CASCADE, null=True)
+    user = models.ForeignKey(
+        settings.AUTH_USER_MODEL,
+        on_delete=models.CASCADE, null=True
+    )
     movie_id = models.IntegerField()
     movie_name = models.CharField(max_length=100)
-
+    
+    class Meta:
+        abstract = True
+    
     def user_name(self):
         return self.user.username
+
+
+class Critic(models.Model):
+    content = models.CharField(max_length=1000)
+
 
 class Vote(models.Model):
-    id = models.UUIDField(primary_key=True, editable=False, default=uuid.uuid4)
     value = models.FloatField()
-    user = models.ForeignKey(settings.AUTH_USER_MODEL,
-                              on_delete=models.CASCADE, null=True)
-    movie_id = models.IntegerField()
-    movie_name = models.CharField(max_length=100)
 
-    def user_name(self):
-        return self.user.username
 
-class Masterpiece(models.Model):
-    id = models.UUIDField(primary_key=True, editable=False, default=uuid.uuid4)
-    position = models.IntegerField()
-    movie_id = models.IntegerField()
-    user = models.ForeignKey(settings.AUTH_USER_MODEL,
-                              on_delete=models.CASCADE, null=True)
+class Masterpiece(MovieBaseModel):
+    pass
+
 
 class Watchlist(models.Model):
-    id = models.UUIDField(primary_key=True, editable=False, default=uuid.uuid4)
-    movie_id = models.IntegerField()
-    user = models.ForeignKey(settings.AUTH_USER_MODEL,
-                              on_delete=models.CASCADE, null=True)
-
-# class Movie(models.Model):
-#     id = models.IntegerField(primary_key=True)
-#     title = models.CharField(max_length=200)
-#     synopsis = models.CharField(max_length=1000)
-#     image_path = models.CharField(max_length=200)
-#     release_date = models.DateField()
-#     duration = models.IntegerField()
+    pass
