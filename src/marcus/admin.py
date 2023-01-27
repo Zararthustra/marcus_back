b0b1f6cad8100cd736743bf118c5713fe64f4bce
Zararthustra <70105093+Zararthustra@users.noreply.github.com>
2023-01-27 21:20:47 +0100

    add admin fields

--- src/marcus/admin.py
+++ src/marcus/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
+from .models import Critic, Vote, Watchlist, Masterpiece
 
-# Register your models here.
+admin.site.register(Critic)
+admin.site.register(Vote)
+admin.site.register(Watchlist)
+admin.site.register(Masterpiece)
